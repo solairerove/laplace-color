0b8e8aae65916770c5c448d89834b793ea01b34e
¨solairerove¨ <hax2033@gmail.com>
2017-02-21 23:10:08 +0300

    GaussianElimination example add

---
+++ src/main/java/com/github/solairerove/theshit/Application.java
@@ -0,0 +1,10 @@
+package com.github.solairerove.theshit;
+
+import com.github.solairerove.theshit.gauss.GaussianElimination;
+
+public class Application {
+    public static void main(String ...args) {
+        GaussianElimination elimination = new GaussianElimination();
+        elimination.main();
+    }
+}
