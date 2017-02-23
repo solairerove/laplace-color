9faa70628f9728e051f33c9da50207123992de98
solairerove <hax2033@gmail.com>
2017-02-23 11:16:07 +0300

    The shit

--- src/main/java/com/github/solairerove/theshit/gauss/GaussianElimination.java
+++ src/main/java/com/github/solairerove/theshit/gauss/GaussianElimination.java
@@ -18,8 +18,21 @@ public class GaussianElimination {
 
 
         // print results
+        System.out.println("=====================");
         for (int i = 0; i < N; i++) {
             StdOut.println(x[i]);
+        }
+    }
+
+    private void print(double[][] A, int N) {
+        System.out.println();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                StdOut.print(A[i][j] + " ");
+                if (j == 2) {
+                    System.out.println();
+                }
+            }
         }
     }
 
@@ -44,12 +57,14 @@ public class GaussianElimination {
             b[p] = b[max];
             b[max] = t;
 
+            this.print(A, N);
+
             // singular or nearly singular
             if (Math.abs(A[p][p]) <= EPSILON) {
                 throw new RuntimeException("Matrix is singular or nearly singular");
             }
 
-            /**
+            /*
             * It first exchanges rows to move the entry with the 
             * largest absolute value to the pivot position
             * Do for all rows below pivot
@@ -57,12 +72,18 @@ public class GaussianElimination {
             **/
             for (int i = p + 1; i < N; i++) {
                 double alpha = A[i][p] / A[p][p];
+                System.out.println("A[i][p] / A[p][p]: " +  A[i][p] + "/" + A[p][p]);
+                System.out.println("alpha: " + alpha);
                 b[i] -= alpha * b[p];
+                System.out.println("b[i]: " + b[i]);
                 // Do for all remaining elements in current row
                 for (int j = p; j < N; j++) {
                     A[i][j] -= alpha * A[p][j];
+                    System.out.println("A[i][j]: " + A[i][j]);
                 }
             }
+
+            this.print(A, N);
         }
 
         // back substitution
